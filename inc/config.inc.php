f1adf40fe67a1f15b5adac6151425e82c62b7af4
hellclanner <hellclanner@d1511ad2-6474-11de-bbe7-2d76dc38abf7>
2009-07-24 05:47:30 +0000

    updated version added customized html tags that are rendered in the templates system.

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -19,7 +19,7 @@ $_SITE = array(
 'name' => 'Samstyle Framework Test Application',
 /* Application name */
 
-'ver' => '1.2.2 BETA',
+'ver' => '1.2.4 BETA',
 /* Application version */
 
 'copyright' => 'Copyright (c) Sam Yong 2008-'.gmdate('Y',strtotime('+1 year')).'. All Rights Reserved.',
