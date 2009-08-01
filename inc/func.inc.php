16dfa7112265f3f859f3f705d85a271c94c8f59f
hellclanner <hellclanner@d1511ad2-6474-11de-bbe7-2d76dc38abf7>
2009-08-01 05:27:03 +0000

    added update checking functionality.

--- inc/func.inc.php
+++ inc/func.inc.php
@@ -23,4 +23,37 @@ return json_encode(array('time'=>gmdate('h:i a',$t)));
 return json_encode(array('date'=>gmdate('jS F Y',$t),'time'=>gmdate('h:i a',$t)));
 }
 
+/* ================================
+getVersions() function
+  checking new versions from Google Code project homepage
+================================ */
+function getVersions(){
+$text = file_get_contents('http://code.google.com/p/samstyle-php-framework/');
+$stable = php::idx(return_between($text, '<strong>Current Stable Version: Samstyle PHP Framework ', '</strong>'),0);
+$dev = php::idx(return_between($text, 'Current development version: <strong>', '</strong>'),0);
+return json_encode(array('stable'=>$stable,'dev'=>$dev));
+}
+
+/* ================================
+return_between() function
+  custom function for getting version number
+================================ */
+function return_between($string, $start, $stop){
+    $st = $string;
+    $list = array();
+    $sl = strlen($start);
+    for($i=0;$i<strlen($string);$i++){
+        $temp = strpos($st, $start);
+        $str = substr($st, $temp+$sl);
+        $split_here = strpos($str, $stop);
+        $parsed_string = substr($str, 0, $split_here);
+        if($parsed_string == '')
+            break;
+        $st = substr($str, $split_here+1);
+        $list[] = $parsed_string;
+    }
+    return $list;
+}
+
+
 ?>
