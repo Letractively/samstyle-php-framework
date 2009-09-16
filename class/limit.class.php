f6bec50f82ebe4717d40350482d8a369fb1dd6f7
hellclanner <hellclanner@d1511ad2-6474-11de-bbe7-2d76dc38abf7>
2009-09-16 14:50:30 +0000

    updated the behavior of firebug->timer_stop() when the timer specified is not set firebug->commit() returns false when the buffer is empty and true when commit operation is successful. added limit class.

---
+++ class/limit.class.php
@@ -0,0 +1,71 @@
+<?php
+if(basename(__FILE__) == basename($_SERVER['PHP_SELF'])){exit();}
+
+/* ****************************************************
+*
+*  Samstyle PHP Framework
+*  Include-only Feature Limiting class
+*  Created by: Sam Yong | Date/Time: 11:08am 19th July 2009 GMT+8
+*
+**************************************************** */
+
+class limit{
+
+private static $sesskey = 'ssphpfw_limitingclass';
+
+private static function init(){
+  if(!isset($_SESSION[self::$sesskey]) && !is_array($_SESSION[self::$sesskey])){
+    $_SESSION[self::$sesskey] = array();
+  }
+}
+
+public static function set($f,$n,$t){
+  self::init();
+  $k = dechex(crc32($f));
+  if(!isset($_SESSION[self::$sesskey][$k])){
+    $_SESSION[self::$sesskey][dechex(crc32($f))] = array('feature'=>$f,'max_counts'=>$n,'clearinterval'=>$t,'counts'=>0,'lastclear'=>time());
+  }
+}
+
+public static function add_count($f){
+  self::init();
+  $k = dechex(crc32($f));
+  if(isset($_SESSION[self::$sesskey][$k])){
+    self::checkreset($f);
+    $_SESSION[self::$sesskey][$k]['counts']++;
+  }
+}
+
+private static function checkreset($f){
+  self::init();
+  $k = dechex(crc32($f));
+  if(isset($_SESSION[self::$sesskey][$k]) && $_SESSION[self::$sesskey][$k]['clearinterval'] > 0 && ($_SESSION[self::$sesskey][$k]['clearinterval']+$_SESSION[self::$sesskey][$k]['lastclear'] < time())){
+    $_SESSION[self::$sesskey][$k]['counts']=0;
+    $_SESSION[self::$sesskey][$k]['lastclear']=time();
+    p(php::dump($_SESSION[self::$sesskey][$k]['clearinterval']+$_SESSION[self::$sesskey][$k]['lastclear'].' '.time()));
+  }
+}
+
+public static function reset($f){
+  self::init();
+  $k = dechex(crc32($f));
+  if(isset($_SESSION[self::$sesskey][$k])){
+     unset($_SESSION[self::$sesskey][$k]);
+  }
+}
+
+public static function check($f){
+  self::init();
+  $k = dechex(crc32($f));
+  if(isset($_SESSION[self::$sesskey][$k])){
+    self::checkreset($f);
+    if($_SESSION[self::$sesskey][$k]['counts']>=$_SESSION[self::$sesskey][$k]['max_counts']){
+       return false;
+    }
+  }
+  return true;
+}
+
+}
+
+?>
