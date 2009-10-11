c36b7e213020b1769335052f1eb07b0d7a5d96e6
hellclanner <hellclanner@d1511ad2-6474-11de-bbe7-2d76dc38abf7>
2009-10-11 04:27:16 +0000

    session fix and improvements

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -62,15 +62,14 @@ if(count($_includes)>0){
 $dn = get_domain($_SITE['approot']);
 if(strtolower($dn) == 'localhost'){$dn='';} // a fix around localhost for cookies.
 session_name(dechex(crc32($_SITE['approot'])));
-session_set_cookie_params((time()+$_SITE['session_length']));
+session_set_cookie_params($_SITE['session_length'],'/');
+ini_set('session.gc_maxlifetime', $_SITE['session_length']);
 $_sessstart = @session_start();
 if(!$_sessstart){
   session_regenerate_id(true); // replace the Session ID
   session_start(); // restart the session (since previous start failed)
 }
-$sess = session_name();
-setcookie($sess, session_id(), time() + $_SITE['session_length']);
-unset($dn);unset($sess);unset($_sessstart); // remove vars
+unset($dn);unset($_sessstart); // remove vars
 
 /* ************************************************
 *   setting out headers
